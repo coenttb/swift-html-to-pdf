641ac572eb43ca2f53a8784be833c35efaa5920e
Coen ten Thije Boonkkamp <coen@tenthijeboonkkamp.com>
2024-07-16 20:05:07 +0200

    wip

--- Tests/HtmlToPdfTests/HtmlToPdfTests.swift
+++ Tests/HtmlToPdfTests/HtmlToPdfTests.swift
@@ -12,24 +12,33 @@ import Testing
 @Suite("Temporary")
 struct TemporaryDirectory {
     
-    @Test func single() async throws {
+    @Test(.disabled()) func single() async throws {
         
-        let id = UUID()
-        let directory = URL.output(id: id)
-        let output = directory
+        await withKnownIssue {
+            
+//           Console log:
+//            FAULT: NSInternalInconsistencyException: Printing failed because PMSessionBeginCGDocumentNoDialog() returned -30872.; {
+//                NSUnderlyingError = "Error Domain=NSCocoaErrorDomain Code=-30872 \"To print this file, you must set up a printer.\" UserInfo={NSLocalizedDescription=To print this file, you must set up a printer., NSLocalizedRecoverySuggestion=To set up a printer, choose Apple menu > System Settings, click Printers & Scanners, and then click the Add (+) button.}";
+//            }
+            
+            let id = UUID()
+            let directory = URL.output(id: id)
+            
+            try directory.createDirectories()
+            
+            try await htmlString.print(to: directory.appendingPathComponent("\(id.uuidString) test string").appendingPathExtension("pdf"), configuration: .a4)
+            
+            let contents_after = try FileManager.default.contentsOfDirectory(at: directory, includingPropertiesForKeys: nil)
+            
+            #expect(contents_after.count == 1)
+            
+            try FileManager.default.removeItems(at: directory)
+        }
         
-        try directory.createDirectories()
         
-        try await htmlString.print(to: directory.appendingPathComponent("\(id.uuidString) test string").appendingPathExtension("pdf"), configuration: .a4)
-        
-        let contents_after = try FileManager.default.contentsOfDirectory(at: directory, includingPropertiesForKeys: nil)
-        
-        #expect(contents_after.count == 1)
-        
-        try FileManager.default.removeItems(at: directory)
     }
     
-    @Test func collection() async throws {
+    @Test func collection_n_size() async throws {
         
         let count = 10
         let output = URL.output()
@@ -46,7 +55,7 @@ struct TemporaryDirectory {
         try FileManager.default.removeItems(at: output)
     }
     
-    @Test func collection_2() async throws {
+    @Test func collection_n_size_double() async throws {
         let count = 10
         
         let output = URL.output()
@@ -65,21 +74,15 @@ struct TemporaryDirectory {
                 to: output, configuration: .a4,
                 filename: { _ in UUID().uuidString }
             )
-        
-        try await htmlString.print(
-            title: UUID().uuidString,
-            to: output,
-            configuration: .a4
-        )
-        
+       
         let contents_after = try FileManager.default.contentsOfDirectory(at: output, includingPropertiesForKeys: nil)
         
-        #expect(contents_after.count == (count * 2) + 1 )
+        #expect(contents_after.count == (count * 2) )
         
         try FileManager.default.removeItems(at: output)
     }
     
-    @Test func collection_3() async throws {
+    @Test func collection_of_documents() async throws {
         
         let output = URL.output()
         
@@ -122,6 +125,39 @@ struct TemporaryDirectory {
         
         try FileManager.default.removeItems(at: output)
     }
+    
+    @Test func collection_collection_individual() async throws {
+        let count = 10
+        
+        let output = URL.output()
+        
+        try output.createDirectories()
+        
+        try await [String].init(repeating: htmlString, count: count)
+            .print(
+                to: output,
+                configuration: .a4,
+                filename: { _ in UUID().uuidString }
+            )
+        
+        try await [String].init(repeating: htmlString, count: count)
+            .print(
+                to: output, configuration: .a4,
+                filename: { _ in UUID().uuidString }
+            )
+        
+        try await htmlString.print(
+            title: UUID().uuidString,
+            to: output,
+            configuration: .a4
+        )
+        
+        let contents_after = try FileManager.default.contentsOfDirectory(at: output, includingPropertiesForKeys: nil)
+        
+        #expect(contents_after.count == (count * 2) + 1 )
+        
+        try FileManager.default.removeItems(at: output)
+    }
 }
 #if os(macOS)
 
@@ -129,20 +165,22 @@ struct TemporaryDirectory {
 struct Local {
     let cleanup: Bool = false
     
-    @Test func individual() async throws {
-        let output = URL.localHtmlToPdf
-        
-        try await htmlString.print(title: "individual", to: output, configuration: .a4)
-        
-        #expect(FileManager.default.fileExists(atPath: output.path))
-        
-        if cleanup {
-            try FileManager.default.removeItems(at: output)
+    @Test(.disabled()) func individual() async throws {
+        await withKnownIssue {
+            let output = URL.localHtmlToPdf.appendingPathComponent("individual")
+            
+            try await htmlString.print(title: "individual", to: output, configuration: .a4)
+            
+            #expect(FileManager.default.fileExists(atPath: output.path))
+            
+            if cleanup {
+                try FileManager.default.removeItems(at: output)
+            }
         }
     }
     
     @Test() func collection() async throws {
-        let output = URL.localHtmlToPdf
+        let output = URL.localHtmlToPdf.appendingPathComponent("collection")
         let count = 10
         
         try output.createDirectories()
