f6bb577fde8b2a97274707096ca2b5416399047b
Coen ten Thije Boonkkamp <coen@tenthijeboonkkamp.com>
2024-07-17 11:54:33 +0200

    wip

--- Sources/HtmlToPdf/macOS.swift
+++ Sources/HtmlToPdf/macOS.swift
@@ -46,13 +46,7 @@ extension [Document] {
                 taskGroup.addTask {
                     for await document in stream {
                         let webView = await webViewPool.acquire()
-                        try await document.html.print(
-                            to: document.url
-                                .deletingPathExtension()
-                                .appendingPathExtension("pdf"),
-                            configuration: configuration,
-                            using: webView
-                        )
+                        try await document.print(configuration: configuration, using: webView)
                         await webViewPool.release(webView)
                     }
                 }
@@ -61,33 +55,131 @@ extension [Document] {
     }
 }
 
-extension Document {
-    /// Prints a single document to a pdf at the given configuration.
-    ///
-    /// This function is more convenient when you have a directory and just want to title the pdf and save it to the directory.
-    ///
-    /// ## Example
-    /// ```swift
-    ///  let html = "<html><body><h1>Hello, World!</h1></body></html>"
-    ///  try await html.print(
-    ///     title: "helloWorld",
-    ///     to: .downloadsDirectory
-    ///  )
-    /// ```
-    ///
-    /// - Parameters:
-    ///   - title: The title of the pdf
-    ///   - directory: The directory at which to print the pdf
-    ///   - configuration: The configuration of the pdf document.
-    ///
-    /// - Throws: `Error` if the function cannot clean up the temporary .html file it creates.
-    public func print(
-       configuration: PDFConfiguration,
-       processorCount: Int = ProcessInfo.processInfo.activeProcessorCount
-    ) async throws {
-        try await [self].print(configuration: configuration, processorCount: processorCount)
+@MainActor
+class WebViewPool {
+    private var pool: [WKWebView]
+    private let semaphore: DispatchSemaphore
+    
+    init(size: Int) {
+        self.pool = (0..<size).map { _ in
+            WKWebView(frame: .zero)
+        }
+        self.semaphore = DispatchSemaphore(value: size)
+    }
+    
+    func acquire() -> WKWebView {
+        semaphore.wait()
+        return pool.removeFirst()
+    }
+    
+    func release(_ webView: WKWebView) {
+        pool.append(webView)
+        semaphore.signal()
     }
 }
+
+extension Document {
+    @MainActor
+    internal func print(
+        configuration: PDFConfiguration,
+        using webView: WKWebView = WKWebView(frame: .zero)
+    ) async throws {
+        
+        let webViewNavigationDelegate = WebViewNavigationDelegate(
+            outputURL: self.url,
+            configuration: configuration
+        )
+        
+        webView.navigationDelegate = webViewNavigationDelegate
+        webView.loadHTMLString(self.html, baseURL: configuration.baseURL)
+        
+        await withCheckedContinuation { continuation in
+            webViewNavigationDelegate.printDelegate = .init {
+                continuation.resume()
+            }
+        }
+    }
+}
+
+class WebViewNavigationDelegate: NSObject, WKNavigationDelegate {
+    private let outputURL: URL
+    var printDelegate: PrintDelegate?
+    
+    private let configuration: PDFConfiguration
+    
+    init(
+        outputURL: URL,
+        onFinished: (@Sendable () -> Void)? = nil,
+        configuration: PDFConfiguration
+    ) {
+        self.outputURL = outputURL
+        self.configuration = configuration
+        self.printDelegate = onFinished.map(PrintDelegate.init)
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        Task { @MainActor [configuration, outputURL, printDelegate] in
+            
+            webView.frame = .init(origin: .zero, size: configuration.paperSize)
+
+            let printOperation = webView.printOperation(with: .pdf(url: outputURL, configuration: configuration))
+                       
+            printOperation.showsPrintPanel = false
+            printOperation.showsProgressPanel = false
+            printOperation.canSpawnSeparateThread = true
+            
+            printOperation.runModal(
+                for: webView.window ?? NSWindow(),
+                delegate: printDelegate,
+                didRun: #selector(PrintDelegate.printOperationDidRun(_:success:contextInfo:)),
+                contextInfo: nil
+            )
+        }
+    }
+}
+
+class PrintDelegate: @unchecked Sendable {
+    
+    var onFinished: @Sendable () -> Void
+    
+    init(onFinished: @Sendable @escaping () -> Void) {
+        self.onFinished = onFinished
+    }
+    
+    @objc func printOperationDidRun(_ printOperation: NSPrintOperation, success: Bool, contextInfo: UnsafeMutableRawPointer?) {
+        // Not sure if this is needed.
+        DispatchQueue.main.async {
+            self.onFinished()
+        }
+    }
+}
+
+
+
+
+
+extension NSEdgeInsets {
+    init(
+        edgeInsets: EdgeInsets
+    ){
+        self = .init(
+            top: edgeInsets.top,
+            left: edgeInsets.left,
+            bottom: edgeInsets.bottom,
+            right: edgeInsets.right
+        )
+    }
+}
+
+
+
+extension CGSize {
+    public static func paperSize()-> CGSize {
+        CGSize(width: NSPrintInfo.shared.paperSize.width, height: NSPrintInfo.shared.paperSize.height)
+    }
+}
+
+
 
 extension String {
     /// Prints a single html string to a pdf at the given directory with the title and margins.
@@ -148,30 +240,6 @@ extension String {
     }
 }
 
-extension String {
-    @MainActor
-    internal func print(
-        to url: URL,
-        configuration: PDFConfiguration,
-        using webView: WKWebView = WKWebView(frame: .zero)
-    ) async throws {
-        
-        let webViewNavigationDelegate = WebViewNavigationDelegate(
-            outputURL: url,
-            configuration: configuration
-        )
-        
-        webView.navigationDelegate = webViewNavigationDelegate
-        webView.loadHTMLString(self, baseURL: configuration.baseURL)
-        
-        await withCheckedContinuation { continuation in
-            webViewNavigationDelegate.printDelegate = .init {
-                continuation.resume()
-            }
-        }
-    }
-}
-
 extension PDFConfiguration {
     public static func a4(margins: EdgeInsets = .a4, baseURL: URL? = nil) -> PDFConfiguration {
         return .init(
@@ -221,93 +289,44 @@ public extension NSPrintInfo {
     }
 }
 
-class WebViewNavigationDelegate: NSObject, WKNavigationDelegate {
-    private let outputURL: URL
-    var printDelegate: PrintDelegate?
-    
-    private let configuration: PDFConfiguration
-    
-    init(
-        outputURL: URL,
-        onFinished: (@Sendable () -> Void)? = nil,
-        configuration: PDFConfiguration
-    ) {
-        self.outputURL = outputURL
-        self.configuration = configuration
-        self.printDelegate = onFinished.map(PrintDelegate.init)
-    }
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        Task { @MainActor [configuration, outputURL, printDelegate] in
-            
-            webView.frame = .init(origin: .zero, size: configuration.paperSize)
-
-            let printOperation = webView.printOperation(with: .pdf(url: outputURL, configuration: configuration))
-                       
-            printOperation.showsPrintPanel = false
-            printOperation.showsProgressPanel = false
-            printOperation.canSpawnSeparateThread = true
-            
-            printOperation.runModal(for: webView.window ?? NSWindow(), delegate: printDelegate, didRun: #selector(PrintDelegate.printOperationDidRun(_:success:contextInfo:)), contextInfo: nil)
-            
-        }
-    }
-}
-
-extension NSEdgeInsets {
-    init(
-        edgeInsets: EdgeInsets
-    ){
-        self = .init(
-            top: edgeInsets.top,
-            left: edgeInsets.left,
-            bottom: edgeInsets.bottom,
-            right: edgeInsets.right
-        )
-    }
-}
-
-class PrintDelegate: @unchecked Sendable {
-    
-    var onFinished: @Sendable () -> Void
-    
-    init(onFinished: @Sendable @escaping () -> Void) {
-        self.onFinished = onFinished
-    }
-    
-    @objc func printOperationDidRun(_ printOperation: NSPrintOperation, success: Bool, contextInfo: UnsafeMutableRawPointer?) {
-        print("printOperationDidRun.success", success)
-        onFinished()
-    }
-}
-
-@MainActor
-class WebViewPool {
-    private var pool: [WKWebView]
-    private let semaphore: DispatchSemaphore
-    
-    init(size: Int) {
-        self.pool = (0..<size).map { _ in
-            WKWebView(frame: .zero)
-        }
-        self.semaphore = DispatchSemaphore(value: size)
-    }
-    
-    func acquire() -> WKWebView {
-        semaphore.wait()
-        return pool.removeFirst()
-    }
-    
-    func release(_ webView: WKWebView) {
-        pool.append(webView)
-        semaphore.signal()
-    }
-}
-
-extension CGSize {
-    public static func paperSize()-> CGSize {
-        CGSize(width: NSPrintInfo.shared.paperSize.width, height: NSPrintInfo.shared.paperSize.height)
-    }
-}
+//extension String {
+//    @MainActor
+//    internal func print(
+//        to url: URL,
+//        configuration: PDFConfiguration
+//    ) async throws {
+//        try await Document.init(url: url, html: self).print(configuration: configuration)
+//    }
+//}
 
 #endif
+
+
+
+//extension Document {
+//    /// Prints a single document to a pdf at the given configuration.
+//    ///
+//    /// This function is more convenient when you have a directory and just want to title the pdf and save it to the directory.
+//    ///
+//    /// ## Example
+//    /// ```swift
+//    ///  let html = "<html><body><h1>Hello, World!</h1></body></html>"
+//    ///  try await html.print(
+//    ///     title: "helloWorld",
+//    ///     to: .downloadsDirectory
+//    ///  )
+//    /// ```
+//    ///
+//    /// - Parameters:
+//    ///   - title: The title of the pdf
+//    ///   - directory: The directory at which to print the pdf
+//    ///   - configuration: The configuration of the pdf document.
+//    ///
+//    /// - Throws: `Error` if the function cannot clean up the temporary .html file it creates.
+//    public func print(
+//       configuration: PDFConfiguration,
+//       processorCount: Int = ProcessInfo.processInfo.activeProcessorCount
+//    ) async throws {
+//        try await [self].print(configuration: configuration, processorCount: processorCount)
+//    }
+//}
